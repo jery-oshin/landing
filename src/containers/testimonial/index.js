265dcfc092aadd3abbc2233ed77b8b97036d8c82
Jery <oshindeveloper12@gmail.com>
2020-04-09 15:14:21 -0400

    Testimonial are working worng but working

--- src/containers/testimonial/index.js
+++ src/containers/testimonial/index.js
@@ -3,24 +3,54 @@ import { Container } from 'reactstrap';
 import { Subtitle, Description, Titlespan2 } from '../../components/title/index';
 import CarouselSlider from './testimonial';
 import './testimonial.scss';
+import { useStaticQuery, graphql } from 'gatsby';
 
 function Testimonial() {
+
+    const data = useStaticQuery(graphql`
+        query {
+            allWordpressWpLandingpages{
+                edges{
+                    node{
+                        id
+                        acf{
+                            testimonial_section_title
+                            testimonial_section_subtitle
+                            testimonial_section_description
+                        }
+                    }   
+                }
+            }
+        }
+    `)
+
     return (
         <section className="testimonial-wrapper gradient-color" id="testimonial">
             <Container>
                 <div className="main-title-wrapper">
                     <Subtitle
                         Class="site-subtitle gradient-color"
-                        Name="testimonial"
+                        Name={data.allWordpressWpLandingpages.edges.map(edge => {
+                            if (edge.node.acf.testimonial_section_subtitle) {
+                                return edge.node.acf.testimonial_section_subtitle
+                            }
+                        })}
                     />
                     <Titlespan2
                         Class="sitemain-subtitle"
-                        Name="Clients"
-                        Label="Feedback"
+                        Name={data.allWordpressWpLandingpages.edges.map(edge => {
+                            if (edge.node.acf.testimonial_section_title) {
+                                return edge.node.acf.testimonial_section_title
+                            }
+                        })}
                     />
                     <Description
                         Class="site-dec"
-                        Name="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown"
+                        Name={data.allWordpressWpLandingpages.edges.map(edge => {
+                            if (edge.node.acf.testimonial_section_description) {
+                                return edge.node.acf.testimonial_section_description
+                            }
+                        })}
                     />
                 </div>
                 <CarouselSlider />
--- src/containers/testimonial/testimonial.js
+++ src/containers/testimonial/testimonial.js
@@ -8,6 +8,74 @@ import "slick-carousel/slick/slick-theme.css";
 import "./testimonial.scss";
 
 function CarouselSlider() {
+    const data = useStaticQuery(graphql`
+        query {
+            allWordpressWpLandingpages{
+                edges{
+                    node{
+                        id
+                        acf{
+                            testimonial_custumer_name
+                            testimonial_custumer_subtitle
+                            testimonial_custumer_description
+                            testimonial_curstumer_image{
+                                source_url
+                            }
+                        }
+                    }   
+                }
+            }
+        }
+    `)
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+    return (
+        <div className="testimonial-slider ">
+            <div className="testimonial-outer-box">
+                <Slider {...settings}>
+                    {data.allWordpressWpLandingpages.edges.map((edge) => {
+                        if(edge.node.acf.testimonial_custumer_name){
+                            return (
+                                <div className="testimonial-item" key={edge.node.id}>
+                                    <div className="testimonial-content">
+                                        <div className="animated-bg">
+                                            <i></i>
+                                            <i></i>
+                                            <i></i>
+                                        </div>
+                                        <Description
+                                            Class="testimonial-dec"
+                                            Name={edge.node.acf.testimonial_custumer_description}
+                                        />
+                                        <Title
+                                            Class="testimonial-title"
+                                            Name={edge.node.acf.testimonial_custumer_name}
+                                        />
+                                        <Title
+                                            Class="testimonial-subtitle"
+                                            Name={edge.node.acf.testimonial_custumer_subtitle}
+                                        />
+                                    </div>
+                                </div>
+                            )
+                        }
+                    })}
+                </Slider>
+            </div>
+
+        </div>
+    );
+}
+
+export default CarouselSlider;
+/*
+function CarouselSlider() {
     const TestimonialList = useStaticQuery(graphql`
         query TestimonialQuery {
             allDataJson {
@@ -72,3 +140,5 @@ function CarouselSlider() {
 }
 
 export default CarouselSlider;
+*/
+
